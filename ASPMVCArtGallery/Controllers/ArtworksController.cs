80de4992772027856863911be734b9b3e5feba05
Philip Greene <PGreene@spartaglobal.com>
2019-09-19 03:46:56 +0100

    initial commit

---
+++ ASPMVCArtGallery/Controllers/ArtworksController.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ASPMVCArtGallery
+{
+    public class ArtworksController : Controller
+    {
+        private ArtGalleryEntities db = new ArtGalleryEntities();
+
+        // GET: Artworks
+        public ActionResult Index()
+        {
+            return View(db.Artworks.ToList());
+        }
+
+        // GET: Artworks/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Artwork artwork = db.Artworks.Find(id);
+            if (artwork == null)
+            {
+                return HttpNotFound();
+            }
+            return View(artwork);
+        }
+
+        // GET: Artworks/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Artworks/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Name,ImgUrl,Price")] Artwork artwork)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Artworks.Add(artwork);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(artwork);
+        }
+
+        // GET: Artworks/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Artwork artwork = db.Artworks.Find(id);
+            if (artwork == null)
+            {
+                return HttpNotFound();
+            }
+            return View(artwork);
+        }
+
+        // POST: Artworks/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Name,ImgUrl,Price")] Artwork artwork)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(artwork).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(artwork);
+        }
+
+        // GET: Artworks/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Artwork artwork = db.Artworks.Find(id);
+            if (artwork == null)
+            {
+                return HttpNotFound();
+            }
+            return View(artwork);
+        }
+
+        // POST: Artworks/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Artwork artwork = db.Artworks.Find(id);
+            db.Artworks.Remove(artwork);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ ASPMVCArtGallery/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ASPMVCArtGallery.MvcApplication" Language="C#" %>
